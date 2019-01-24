faa300f428f97593aa99fc72e542cceda458ca63
Joshua Horowitz <joshua.horowitz@berkeley.edu>
2019-01-24 09:22:05 -0800

    start with logos and the card panel

--- flash-app/src/components/Card.jsx
+++ flash-app/src/components/Card.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import '../css/card.css'
+import '../css/typography.css'
+import '../css/icons.css'
 
 class Card extends Component {
 
@@ -15,11 +17,20 @@ class Card extends Component {
   render() {
     return (
       <div className="card app-container">
-      
+
         <div className='card__panel'>
           <a href="localhost:3000">
             <div className="card__panel__logo"></div>
           </a>
+          <div className="card__panel__banner">
+            <span>
+              Wordz with Friendz: Lesson 1
+            </span>
+          </div>
+          <div className="card__panel__control-panel">
+            <a href="localhost:3000"><i type="glyph-cross-soft"></i></a>
+            <div className="icon icon--cancel"></div>
+          </div>
         </div>
 
         <div className='card__screens'>
