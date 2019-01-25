5d98398a825f31bed4c1ca0230e7c173222a4549
Joshua Horowitz <joshua.horowitz@berkeley.edu>
2019-01-24 22:48:35 -0800

    Updates

---
+++ precache-manifest.a1264d801cd19184dc0b0e3c703f0111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77fca812286b9747b4fd6d97570ca8dd",
+    "url": "www.flashpopz.com/static/media/baseline-cancel-24px.77fca812.svg"
+  },
+  {
+    "revision": "1fcbe1f258899b129294",
+    "url": "www.flashpopz.com/static/js/runtime~main.1fcbe1f2.js"
+  },
+  {
+    "revision": "55b143cfe72d3162888f",
+    "url": "www.flashpopz.com/static/js/main.55b143cf.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "www.flashpopz.com/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "55b143cfe72d3162888f",
+    "url": "www.flashpopz.com/static/css/main.e23bd38e.chunk.css"
+  },
+  {
+    "revision": "38a0c4255ff487a37547d01e129c191d",
+    "url": "www.flashpopz.com/index.html"
+  }
+];
